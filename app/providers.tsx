88005344cda8324ae5541a52936003acf7b0194e
oumaoumag <ouma.godwin10@gmail.com>
2025-08-14 22:51:24 +0300

    Update providers with MiniKit configuration for Farcaster integration

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { type ReactNode } from "react";
-import { base } from "wagmi/chains";
+import { base } from "viem/chains";
 import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
 import { RewardsProvider } from "./components/context/RewardsContext";
 import { ToastProvider } from "./components/context/ToastContext";
